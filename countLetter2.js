b13947e22fa2faf02cbceac38118275b5ffe07eb
Terence1991 <cplterencehawes@gmail.com>
2018-09-26 20:27:08 +0000

    first commit

---
+++ countLetter2.js
@@ -0,0 +1,30 @@
+function countLetters(str) {
+    // asign emty object to add on keys and values     
+    var obj = {};
+    // split input string to loop through and asign 
+    var keyString = str.split("");
+    
+    
+    // loop that loops through Keystring and asigns Index[i] keys of obj 
+    for (i = 0; i < keyString.length; i++) {
+       let letter = keyString[i];
+       
+    
+
+/// if else check if keys exist. If it exist add 1 
+if (obj[letter]) {
+    obj[letter].push(i)
+} else { 
+  obj[letter] = [i];
+
+}
+
+    }
+
+return obj;
+    
+    };
+
+console.log(countLetters("lighthouse in the house"));
+
+
